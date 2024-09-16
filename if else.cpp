7b54d8d85bb9819aff16a6b2de672a3a44dec5e7
QMASPalencia <qmaspalencia@tip.edu.ph>
2024-09-16 11:25:43 +0800

    Add files via upload

---
+++ if else.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+int x,y;
+
+cout<<"Please input 2 integers on the following"<<endl;
+cout<<"Please input your x value: ";cin>>x;
+cout<<"Please input your y value: ";cin>>y;
+
+if(x>y)
+{
+	
+	cout<<"x is greater than y";
+	
+}
+else if(x<y)
+{
+	
+	cout<<"x is less than y";
+	
+}
+
+else
+{
+	
+	cout<<"x is equal to y";
+		
+}
+
+return 0;
+}
---
+++ while.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+main()
+{
+	int i=0;
+	
+	while(i<=10)
+	{
+		
+		cout<<"Hello "<<i<<endl;
+		
+		i=i+1;
+		
+	}
+	
+	return 0;
+}
