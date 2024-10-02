4565fe0c3af831a3b2b48d33206b1bc7999890db
QMASPalencia <qmaspalencia@tip.edu.ph>
2024-10-02 12:59:33 +0800

    Add files via upload

---
+++ Practical Lab.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+
+int main()
+{
+	
+	int i;
+	int a=0;
+	int j=5;
+	int n=5;
+	
+	for(i=1;i<=10;i++)
+	{
+		n=n-1;
+		a=a+n;
+		
+	}
+	
+	cout<<a<<endl;
+	
+	return 0;
+}
