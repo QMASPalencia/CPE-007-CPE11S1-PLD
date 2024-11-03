d05728c4b60884363d9d4b45924b20b58f803cb5
QMASPalencia <qmaspalencia@tip.edu.ph>
2024-11-04 01:05:49 +0800

    Update Activity12_3_Palencia.cpp

--- Activity12_3_Palencia.cpp
+++ Activity12_3_Palencia.cpp
@@ -3,12 +3,19 @@ using namespace std;
 
 int main()
 {
-	char in[100];
-	int i,input;
-	for(i=0;i<4;i++)
+	string in;
+	double input=0,total=0;
+	cout<<"Please input 4 words : "<<endl;
+	for (int i = 0; i < 4; i++)
 	{
-		cout<<"Please input word "<< i+1<<": ";cin>>in[i];
+	cout<<i+1<<": ";cin>>in;
+		for (int j = 0; j < in.size(); j++)
+    	{
+    		input = (int)in[j];
+    		total = total + input;
+    	}
 	}
 	
-	
+	cout<<"The sum of the words is: "<<total;
+	return 0;
 }
